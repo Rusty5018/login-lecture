796dbe193bd6be98f4601c32318190c4486c8760
rusty5018 <rusty5018@naver.com>
2022-11-18 22:00:41 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,6 +20,13 @@ const id = document.querySelector("#id"),
           body: JSON.stringify(req),
       }).then((res) => res.json())
         .then((res) => {
-            
-        });
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("로그인 중 에러 발생");
+        })
   }

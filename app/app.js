67671daf2172ca9e4e8e3c96f63d4d593debc4f9
[Rusty5018] <[rusty5018@naver.com]>
2022-11-18 11:55:15 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -10,9 +10,8 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
 // use() -> 미들웨어를 등록하는 메소드
-app.use("/", home) ;
 app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home) ;
 
 module.exports = app;

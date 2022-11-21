1fb6e7b127e958ccb6b3046bd8cb6242f99cd80d
[Rusty5018] <[rusty5018@naver.com]>
2022-11-21 16:03:37 +0900

    User모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const { getUsers } = require('../../models/UserStorage');
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -13,22 +13,9 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
-    
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id)
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
+ 
         return res.json(response);
      },
 };
